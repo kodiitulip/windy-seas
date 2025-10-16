332e1f79984994db0c0652d6975c06a27d5a9e9f
kodiitulip <kodii.tulip@gmail.com>
2025-10-16 17:11:54 -0300

    feat: changed boat controls

--- scenes/objects/boat/boat.gd
+++ scenes/objects/boat/boat.gd
@@ -4,6 +4,10 @@ extends RigidBody3D
 @export var max_speed: float = 10.0
 @export var flip_foward: bool = false
 
+@export var acceleration_rate: float = 8.0
+@export var deacceleration_rate: float = 12.0
+@export var brake_power: float = 20.0
+
 var curr_speed: float = 0.0
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +15,16 @@ func _physics_process(delta: float) -> void:
 
 
 func _handle_move(delta: float) -> void:
-	if Input.is_action_pressed(&"accelerate"):
-		curr_speed = move_toward(curr_speed, max_speed, delta * 2.5)
-	if Input.is_action_pressed(&"deaccelerate"):
-		curr_speed = move_toward(curr_speed, 0.0, delta * 2.5)
+	var accel: float = Input.get_axis(&"deaccelerate", &"accelerate")
+	if is_zero_approx(accel):
+		curr_speed = move_toward(curr_speed, 0.0, delta * deacceleration_rate)
+	elif (curr_speed > 0 and accel < 0) or (curr_speed < 0 and accel > 0):
+		curr_speed = move_toward(curr_speed, 0.0, delta * brake_power)
+	else:
+		curr_speed = move_toward(curr_speed, max_speed * accel, delta *
+				acceleration_rate)
 	if Input.is_action_just_pressed(&"toggle_anchor"):
-		curr_speed = 0.0
+		get_tree().create_tween().tween_property(self, ^"curr_speed", 0.0, .2)
 	var dir: Vector3 = Vector3.BACK if flip_foward else Vector3.FORWARD
 	apply_central_force(global_transform.basis * dir * curr_speed)
 	var rotation_dir: float = -Input.get_axis(&"turn_left", &"turn_right")
